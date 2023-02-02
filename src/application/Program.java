43f83248dc7fd836afd254f87fac6de850add901
Pedro Bruno <pbruno1283@gmail.com>
2023-02-02 10:24:41 -0300

    UpdateSeller implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,9 +30,15 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n---- TEST 3 : seller Insert ----");
+        System.out.println("\n---- TEST 4 : seller Insert ----");
         Seller newSeller = new Seller(null, "Pedro Bruno", "pedro@gmail2.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + seller.getId());
+
+        System.out.println("\n---- TEST 5 : seller Update ----");
+        seller =sellerDao.findById(1);
+        seller.setName("Joao Paulo");
+        sellerDao.update(seller);
+        System.out.println("Update complete! New name = " + seller.getName());
     }
 }
